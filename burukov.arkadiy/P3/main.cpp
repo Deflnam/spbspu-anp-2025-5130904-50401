525e12d4c4492ddf304d9dd46fb56ce123d37985
Deflnam <burukov01@bk.ru>
2025-11-13 22:28:35 +0300

    Creating and deleting a matrix; and namespace added

--- burukov.arkadiy/P3/main.cpp
+++ burukov.arkadiy/P3/main.cpp
@@ -1,6 +1,27 @@
 #include "iostream"
+#include <cstddef>
+#include <cstdlib>
+
+namespace burukov
+{
+  void destroyMatrix(int* matrix);
+  int* createMatrix(size_t rows, size_t cols);
+}
+
+void burukov::destroyMatrix(int* matrix)
+{
+  free(matrix);
+}
+
+int* burukov::createMatrix(size_t rows, size_t cols)
+{
+  int* matrix = nullptr;
+  matrix = static_cast<int*>(malloc(rows * cols * sizeof(int)));
+  return matrix;
+}
+
 
 int main()
 {
-    
+
 }
